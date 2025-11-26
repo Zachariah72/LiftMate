2d2961f3fc67aebcc07879ba06a284c34bc4289a
Zachariah72 <zacmanani20@gmail.com>
2025-11-27 00:03:56 +0300

    Add static file serving for frontend images

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
@@ -10,6 +11,7 @@ const paymentRoutes = require('./routes/payment');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
